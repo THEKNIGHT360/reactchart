279aa0fbe37378935c106832e37b3298b6f78bda
THEKNIGHT360 <pranzalhero@gmai.com>
2021-01-16 17:06:36 +0545

    Updates

---
+++ precache-manifest.e33fbcc9f25bd49fbb884e47655f809e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db76d33a92f7cb1ed213cd508117173",
+    "url": "/reactchart/index.html"
+  },
+  {
+    "revision": "b10496ae889c2dfae879",
+    "url": "/reactchart/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "8e9f368f1227c20feee5",
+    "url": "/reactchart/static/js/2.68a19e2e.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/reactchart/static/js/2.68a19e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10496ae889c2dfae879",
+    "url": "/reactchart/static/js/main.5ef1dc69.chunk.js"
+  },
+  {
+    "revision": "a40f7ad68aa28d2d079a",
+    "url": "/reactchart/static/js/runtime-main.20c016ff.js"
+  }
+]);
